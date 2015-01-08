407d23f2e064c56a5c5aefb06e5bfc37a5ef38dc
gabe <GabeMaker@users.noreply.github.com>
2015-01-08 18:48:54 +0000

    ex17 drill 1 - 7 lines but shortened from original and still readable

--- ex10-19/ex17_1.rb
+++ ex10-19/ex17_1.rb
@@ -1,1 +1,7 @@
-from_file, to_file = ARGV; indata = open(from_file).read; out_file = open(to_file, 'w'); out_file.write(indata); out_file.close; puts "Copy of #{from_file} made in #{to_file}"
+from_file, to_file = ARGV
+indata = open(from_file).read
+
+out_file = open(to_file, 'w')
+out_file.write(indata); out_file.close
+
+puts "Copy of #{from_file} made in #{to_file}"
