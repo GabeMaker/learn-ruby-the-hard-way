a18e05849171ffa75894053822b8db5c31c2c031
gabe <GabeMaker@users.noreply.github.com>
2015-01-08 18:25:04 +0000

    ex2, reviewed ex17 - pre study drills

--- ex17.rb
+++ ex17.rb
@@ -2,19 +2,29 @@ from_file, to_file = ARGV
 
 puts "copying from #{from_file} to #{to_file}"
 
-in_file = open(from_file)
-indata = in_file.read
+# we could do these two on one line. How?
+# in_file = open(from_file)
+# indata = in_file.read
+indata = open(from_file).read
 
-puts "the input file is #{indata.length} bytes long"
+# open()  - takes a string (representing a file in current directory) and returns a file object
+# read    - method is called on an IO object (the file) and returns a string (contents of the file)
+# (the File class is a subclass of the IO class)
 
-puts "does the output file exist? #{File.exist?(to_file)}"
-puts "Ready, hit RETURN to continue, CTRL-C to abort."
+puts "The input file is #{indata.length} bytes long"
+
+# (File.exist?("file") returns true if there is a file called "file" in the current directory)
+puts "Does the output file exist? #{File.exist?(to_file)}"
+puts "Ready, hit return to continue, CTRL-C to abort."
 $stdin.gets
+
+# open( ,'w')   - creates a file if one does not exist
+# write('str')  - callen on File object and writes given string into it       
 
 out_file = open(to_file, 'w')
 out_file.write(indata)
 
-puts "done"
+puts "Alright, all done."
 
 out_file.close
-in_file.close
+# in_file.close
