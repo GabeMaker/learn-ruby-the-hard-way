decc17e5937bc7983f4ecbaf48d4bf1c228712be
gabe <GabeMaker@users.noreply.github.com>
2015-01-20 10:19:02 +0000

    ex40 added line count and naming methods

--- ex40-49/ex40.rb
+++ ex40-49/ex40.rb
@@ -2,10 +2,23 @@ class Song
 
   def initialize(lyrics)
     @lyrics = lyrics
+    @name = "(This song is currently unnamed)"
+  end
+
+  def add_name(name)
+    @name = name
   end
 
   def sing_me_a_song()
     @lyrics.each {|line| puts line }
+  end
+
+  def line_count
+    if @name != "(This song is currently unnamed)"
+      puts "#{@name} has #{@lyrics.count} lines"
+    else
+      puts "This song has #{@lyrics.count} lines " + @name
+    end
   end
 end
 
@@ -20,3 +33,7 @@ fresh_prince.sing_me_a_song
 lyrics = ["Such a perfect day", "Drink sangria in the park","Then later when it gets dark","We go home"]
 perfect_day = Song.new(lyrics)
 perfect_day.sing_me_a_song
+
+# Study drill 3
+perfect_day.add_name("Perfect Day")
+perfect_day.line_count
